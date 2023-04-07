7c0b901c9440489865e38232bcb987aeffec337b
Minipro3217 <minamilad213@gmail.com>
2023-04-07 17:36:09 +0300

    Wrote the initial finished code

--- workout.py
+++ workout.py
@@ -1,8 +1,27 @@
 import csv
 import datetime
+import os
+
+filename = "workout_log.csv"
+if not os.path.exists(filename):
+    field_names_list = []
+    print("""Enter the name of exercises (put a space after each exercise) you want to add (type "close" to end): """)
+    while True:
+        field_names=input()
+        if field_names=="close":
+            break
+        else:
+            fields = field_names.split()
+            field_names_list.extend(fields)
+    with open(filename, mode="a") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONE)
+        writer.writerow(field_names_list)
+
+with open(filename, "r") as file:
+    firstline_file=file.readline()
 
 date = datetime.date.today()
-fields = ['Weighted_squats', 'Pullups', 'Pushups', 'Plank', 'Lunges', 'Crunches', 'Hold','Curls']
+fields = firstline_file.split(',')
 values = []
 
 for field in fields:
